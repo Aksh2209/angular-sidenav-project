f9d61ba63940268d087ef023b26a630fb675fce7
Aksh2209 <akshayap672@gmail.com>
2020-06-19 17:14:32 +0530

    2 files modified

--- src/app/global/header/header.component.ts
+++ src/app/global/header/header.component.ts
@@ -9,9 +9,7 @@ import { Component } from '@angular/core';
           <img class="logo" src="https://deplomatic-ui.s3.amazonaws.com/assets/logo/mySandbox_logo_dark_bg.svg" placeholder="Logo" height="30px" >
         </div>
         <nav class="nav-links">
-          <a href="#Ticket">Tickets</a>
-          <a href="#Approval">Approvals</a>
-          <a href="#Environment">Environments</a>
+          <a href="#Home">Tickets</a>
         </nav>
       </section>
     </header>
